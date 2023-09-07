f5e018cfcda5c327cbbccf60caa4dc1decf4424a
seanboss-bit <shaunaibek@gmail.com>
2023-09-07 16:16:11 +0100

    new whatsapp

--- components/Whatsapp.js
+++ components/Whatsapp.js
@@ -4,7 +4,7 @@ import styles from "../src/styles/cart.module.css";
 const Whatsapp = () => {
   return (
     <div className={styles.whatsapp}>
-      <Link target="_blank" href={"https://wa.me/2348058538362"}   rel="noreferrer">
+      <Link target="_blank" href={"https://wa.me/2348129198327"}   rel="noreferrer">
         <i className="fa-brands fa-whatsapp"></i>
       </Link>
     </div>

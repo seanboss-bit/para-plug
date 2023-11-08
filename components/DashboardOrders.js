a29af5fcb8a77c97300e102f50f4ad28fde1574d
seanboss-bit <shaunaibek@gmail.com>
2023-11-08 19:34:59 +0100

    new dashboard

--- components/DashboardOrders.js
+++ components/DashboardOrders.js
@@ -1,13 +1,19 @@
+"use client";
+import { useState } from "react";
 import styles from "../src/styles/dashboard.module.css";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
 const DashboardOrders = () => {
+  const [showDetails, setShowDetails] = useState(false);
+  function numberWithCommas(x) {
+    return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   return (
     <div className={styles.orderMain}>
       <div className="container">
         <div>
           <div className={styles.orderHeading}>
-            <h2>recent orders</h2>
-            <a href="#">view all</a>
+            <h2>all orders</h2>
           </div>
           <div className={styles.orderrow}>
             <div className={styles.orderbox}>
@@ -24,7 +30,9 @@ const DashboardOrders = () => {
                 <span className={styles.price}>NGN 1,000</span>
                 <div className={styles.sum}>
                   <p>4th August 2023</p>
-                  <button>view details</button>
+                  <button onClick={() => setShowDetails(true)}>
+                    view details
+                  </button>
                 </div>
               </div>
             </div>
@@ -60,7 +68,9 @@ const DashboardOrders = () => {
                 <span className={styles.price}>NGN 1,000</span>
                 <div className={styles.sum}>
                   <p>4th August 2023</p>
-                  <button>view details</button>
+                  <button onClick={() => setShowDetails(true)}>
+                    view details
+                  </button>
                 </div>
               </div>
             </div>
@@ -85,6 +95,49 @@ const DashboardOrders = () => {
           </div>
         </div>
       </div>
+      {showDetails && (
+        <div className={styles.detbg}>
+          <div className={styles.detbody}>
+            <div className={styles.cl}>
+              <XMarkIcon onClick={() => setShowDetails(false)} />
+            </div>
+            <div>
+              <div className={styles.titles}>
+                <h3 className={styles.productitile}>product</h3>
+                <h3 className="price">price</h3>
+                <h3 className="quantity">quantity</h3>
+                <h3 className={styles.total}>total</h3>
+              </div>
+              <div className={styles?.cartItem}>
+                <div className={styles?.cartproduct}>
+                  <img
+                    src={
+                      "https://res.cloudinary.com/dvo4tlcrx/image/upload/v1697713286/r2lm9mkmxdl1egw2dwcx.png"
+                    }
+                    alt="#"
+                  />
+                  <div>
+                    <h3>shoe</h3>
+                    <p>jordan</p>
+                    <p>size : 43</p>
+                  </div>
+                </div>
+                <div className={styles.cartprice}>
+                  NGN {numberWithCommas(4000)}
+                </div>
+                <div className={styles.res}>
+                  <div className={styles.cartquantity}>
+                    <div className="count">4</div>
+                  </div>
+                </div>
+                <div className={styles.carttotal}>
+                  NGN {numberWithCommas(9000)}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- components/Refferal.js
+++ components/Refferal.js
@@ -1,9 +1,62 @@
-
+import styles from "../src/styles/dashboard.module.css";
+import { DocumentDuplicateIcon } from "@heroicons/react/24/solid";
 
 const Refferal = () => {
   return (
-    <div>Refferal</div>
-  )
-}
+    <div>
+      <div className="container">
+        <div className={styles.refInner}>
+          <h2>referal system</h2>
+          <p>
+            refer 10 people and get a free sneaker from our sneaker collection
+          </p>
+        </div>
+        <div className={styles.refBody}>
+          <h3>
+            your referals <span>3/10</span>
+          </h3>
+          <p>
+            your <span>seven</span> steps away from claiming a free sneaker
+          </p>
 
-export default Refferal
+          <div className={styles.progressBody}>
+            <div className={styles.progress}>
+              <div className={styles.progressBar}></div>
+            </div>
+            <span>3/10 referals made</span>
+          </div>
+
+          <h4 className={styles.shareMain}>share link</h4>
+          <span className={styles.shareText}>
+            share your referal link to get all kicked up
+          </span>
+
+          <div className={styles.ref}>
+            <div className={styles.shareLink}>
+              <div className={styles.link}>
+                <span>https://paraplug.store?ref=123</span>
+                <button>
+                  <DocumentDuplicateIcon />
+                  copy link
+                </button>
+              </div>
+            </div>
+            <div className={styles.socialRow}>
+              <div className={styles.socialBox}>
+                <i className="fa-brands fa-instagram"></i>
+              </div>
+              <div className={styles.socialBox}>
+                <i className="fa-brands fa-instagram"></i>
+              </div>
+              <div className={styles.socialBox}>
+                <i className="fa-brands fa-instagram"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Refferal;

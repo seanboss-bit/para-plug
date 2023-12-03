6caa7295b9ed0eedc912481f440bcdaad0b67d78
seanboss-bit <shaunaibek@gmail.com>
2023-12-03 14:42:18 +0100

    well

--- components/Product.js
+++ components/Product.js
@@ -1,15 +1,18 @@
 "use client";
 import Image from "next/image";
 import styles from "../src/styles/product.module.css";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { addProduct } from "@/redux/features/cartReducer";
 import { publicRequest } from "../requests";
 import Loading from "./Loading";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const Product = () => {
+  const router = useRouter();
   const [loading, setLoading] = useState(false);
   function numberWithCommas(x) {
     return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -55,85 +58,106 @@ const Product = () => {
         {loading ? (
           <Loading loading={loading} />
         ) : (
-          <div className={styles.product}>
-            <div className={styles.productimages}>
-              <img
-                src={bigimg === undefined ? singleItem?.image : bigimg}
-                alt="#"
-                className={styles.mainimg}
-              />
-              <div className={styles.subimages}>
-                {singleItem?.extraImg.map((img, i) => (
-                  <Image
-                    key={i}
-                    width={100}
-                    height={100}
-                    src={img}
-                    alt="#"
-                    objectFit="contain"
-                    onClick={() => activePic(i)}
-                    className={
-                      singleItem?.extraImg[i] === bigimg ? styles.active : null
-                    }
-                  />
-                ))}
-              </div>
-            </div>
-            <div className={styles.info}>
-              <div>
-                <span className={styles.location}>
-                  store / {singleItem?.category} / {singleItem?.name}
-                </span>
-                <p className={styles.cat}>{singleItem?.category}</p>
-                <h3 className={styles.infoname}>{singleItem?.name}</h3>
-                <p className={styles.pl}>
-                  {singleItem?.slashPrice ? (
-                    <span>NGN {numberWithCommas(singleItem?.slashPrice)}</span>
-                  ) : null}
-                  <p>NGN {numberWithCommas(singleItem?.price)}</p>
-                  {singleItem?.freeShipping ? (
-                    <span>+ free shipping</span>
-                  ) : null}
-                </p>
-                <p className={styles.desc}>{singleItem?.description}</p>
-              </div>
-              <div>
-                <div className={styles.options}>
-                  {/* <input type="number" placeholder={1} defaultValue={1} /> */}
-                  <select onChange={(e) => setSize(e.target.value)}>
-                    <option value={"size"}>size</option>
-                    {singleItem?.sizes.map((size, i) => (
-                      <option key={i} value={size}>
-                        {" "}
-                        {size}
-                      </option>
-                    ))}
-                  </select>
+          <>
+            <Link href={'/store'} className={styles.back}>
+              <ArrowUturnLeftIcon />
+              <span>back</span>
+            </Link>
+            <div className={styles.product}>
+              <div className={styles.productimages}>
+                <img
+                  src={bigimg === undefined ? singleItem?.image : bigimg}
+                  alt="#"
+                  className={styles.mainimg}
+                />
+                <div className={styles.subimages}>
+                  {singleItem?.extraImg.map((img, i) => (
+                    <Image
+                      key={i}
+                      width={100}
+                      height={100}
+                      src={img}
+                      alt="#"
+                      objectFit="contain"
+                      onClick={() => activePic(i)}
+                      className={
+                        singleItem?.extraImg[i] === bigimg
+                          ? styles.active
+                          : null
+                      }
+                    />
+                  ))}
                 </div>
-                {user ? (
-                  <>
-                    {" "}
-                    {singleItem?.inStock ? (
-                      <button
-                        className={styles.btn}
-                        onClick={() => add(singleItem)}
-                      >
-                        add to cart
-                      </button>
-                    ) : (
-                      <div className={styles.out}>out of stock</div>
-                    )}
-                  </>
-                ) : (
-                 <p style={{
-                  margin: '40px 0px',
-                  fontSize: "20px",
-                  textAlign: "center"
-                 }}>Please Login To Buy A Kick</p>
-                )}
+              </div>
+              <div className={styles.info}>
+                <div>
+                  <span className={styles.location}>
+                    store / {singleItem?.category} / {singleItem?.name}
+                  </span>
+                  <p className={styles.cat}>{singleItem?.category}</p>
+                  <h3 className={styles.infoname}>{singleItem?.name}</h3>
+                  <p className={styles.pl}>
+                    {singleItem?.slashPrice ? (
+                      <span>
+                        NGN {numberWithCommas(singleItem?.slashPrice)}
+                      </span>
+                    ) : null}
+                    <p>NGN {numberWithCommas(singleItem?.price)}</p>
+                    {singleItem?.freeShipping ? (
+                      <span>+ free shipping</span>
+                    ) : null}
+                  </p>
+                  <p className={styles.desc}>{singleItem?.description}</p>
+                </div>
+                <div>
+                  <div className={styles.options}>
+                    {/* <input type="number" placeholder={1} defaultValue={1} /> */}
+                    <select onChange={(e) => setSize(e.target.value)}>
+                      <option value={"size"}>size</option>
+                      {singleItem?.sizes.map((size, i) => (
+                        <option key={i} value={size}>
+                          {" "}
+                          {size}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  {user ? (
+                    <>
+                      {" "}
+                      {singleItem?.inStock ? (
+                        <button
+                          className={styles.btn}
+                          onClick={() => add(singleItem)}
+                        >
+                          add to cart
+                        </button>
+                      ) : (
+                        <div className={styles.out}>out of stock</div>
+                      )}
+                    </>
+                  ) : (
+                    <p
+                      style={{
+                        margin: "40px 0px",
+                        fontSize: "20px",
+                        textAlign: "center",
+                        cursor: "pointer",
+                        background: "#0084d6",
+                        color: "#fff",
+                        padding: "10px",
+                      }}
+                      onClick={() => {
+                        router.push("/login");
+                      }}
+                    >
+                      Please Login To Buy A Kick
+                    </p>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
+          </>
         )}
       </div>
     </div>

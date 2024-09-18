7baa61ae1a3abc6334a7fa455ae8e31021c25921
seanboss-bit <shaunaibek@gmail.com>
2024-09-18 09:20:36 +0100

    favicon update

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -2,13 +2,6 @@
 import Footer from "../../../components/Footer";
 import Navbar from "../../../components/Navbar";
 import StoreBody from "../../../components/StoreBody";
-export const metadata = {
-  icons: {
-    icon: "./para.png",
-    shortcut: "./para.png",
-    apple: "./para.png",
-  },
-};
 
 const page = () => {
   return (

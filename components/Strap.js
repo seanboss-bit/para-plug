977613f11eefe46ae33daf29f8639934639f4782
seanboss-bit <shaunaibek@gmail.com>
2024-12-13 12:49:03 +0100

    christmas decor

--- components/Strap.js
+++ components/Strap.js
@@ -1,7 +1,11 @@
 import styles from "../src/styles/scroll.module.css";
 
 const Strap = () => {
-  return <div className={styles.container}>get free acrylic shoe box. Shop now.</div>;
+  return (
+    <div className={styles.container}>
+      <p>get free acrylic shoe box. Shop now.</p>
+    </div>
+  );
 };
 
 export default Strap;
